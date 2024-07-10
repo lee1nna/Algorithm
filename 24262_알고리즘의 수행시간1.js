7965f1f2bf6f880527f4c17e464f6b956c5e485e
lee1nna <dlgkssk0209@gmail.com>
2024-07-10 17:09:38 +0900

    Add: 24262_알고리즘의 수행시간1

---
+++ 24262_알고리즘의 수행시간1.js
@@ -0,0 +1,11 @@
+// const input = +require("fs")
+//   .readFileSync(
+//     process.platform === "linux"
+//       ? "/dev/stdin"
+//       : "./24262_알고리즘의 수행시간1.txt"
+//   )
+//   .toString()
+//   .trim();
+
+console.log(1);
+console.log(0);
