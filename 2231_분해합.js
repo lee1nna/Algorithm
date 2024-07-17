2ac9e19d4da2da501b3b5ce9648fe13e6a3211de
lee1nna <dlgkssk0209@gmail.com>
2024-07-17 15:42:51 +0900

    Solve:2231_분해합

---
+++ 2231_분해합.js
@@ -0,0 +1,24 @@
+const input = require("fs")
+  .readFileSync(
+    process.platform === "linux" ? "/dev/stdin" : "./2231_분해합.txt"
+  )
+  .toString()
+  .trim();
+
+let creator = [];
+
+for (let i = +input; i > 0; i--) {
+  let sum = i;
+
+  for (let j = 0; j < input.length; j++) {
+    if (String(i)[j]) {
+      sum += Number(String(i)[j]);
+    }
+  }
+
+  if (sum === +input) {
+    creator.push(i);
+  }
+}
+
+console.log(Math.min(...creator));
