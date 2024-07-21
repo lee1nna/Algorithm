c57bda5c0d46fbe1d95b70e28e976ea817155425
lee1nna <dlgkssk0209@gmail.com>
2024-07-21 15:09:18 +0900

    Solve: 1018_체스판다시칠하기 리팩토링

--- 1018_체스판다시칠하기.js
+++ 1018_체스판다시칠하기.js
@@ -34,28 +34,31 @@ for (let i = 0; i < 8; i++) {
   }
 }
 
-let compareW = 0;
-let compareB = 0;
-let min = 0;
+function compareChess(i, j) {
+  let compareW = 0;
+  let compareB = 0;
+  for (let k = 0; k < 8; k++) {
+    for (let m = 0; m < 8; m++) {
+      //WChess
+      if (WChess[k][m] !== board[k + i][m + j]) {
+        compareW += 1;
+      }
+      if (BChess[k][m] !== board[k + i][m + j]) {
+        compareB += 1;
+      }
+    }
+  }
+
+  return Math.min(compareB, compareW);
+}
+
+let min = 100;
 
 for (let i = 0; i <= M - 8; i++) {
   for (let j = 0; j <= N - 8; j++) {
-    for (let k = 0; k < 8; k++) {
-      for (let m = 0; m < 8; m++) {
-        //WChess
-        if (WChess[k][m] !== board[k + i][m + j]) {
-          compareW += 1;
-        }
-        if (BChess[k][m] !== board[k + i][m + j]) {
-          compareB += 1;
-        }
-      }
-    }
+    chessResult = compareChess(i, j);
 
-    min = Math.min(compareB, compareW, min);
-
-    compareW = 0;
-    compareB = 0;
+    min = Math.min(chessResult, min);
   }
 }
 
