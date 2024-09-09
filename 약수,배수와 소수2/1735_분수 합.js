397a78107f52f604318f52083b858b8da6b9897c
lee1nna <dlgkssk0209@gmail.com>
2024-09-09 23:47:44 +0900

    Solve3: 1735_분수합

--- 약수,배수와 소수2/1735_분수 합.js
+++ 약수,배수와 소수2/1735_분수 합.js
@@ -10,20 +10,12 @@ const [num1, deno1] = input[0].split(" ").map((el) => +el);
 const [num2, deno2] = input[1].split(" ").map((el) => +el);
 
 const gcdFunc = (a, b) => {
-  let min = a > b ? b : a;
-  let temp = 0;
-
-  for (let i = 2; i <= min; i++) {
-    if (a % i === 0 && b % i === 0) {
-      temp = i;
-    }
-  }
-
-  return temp;
+  if (b === 0) return a;
+  else return gcdFunc(b, a % b);
 };
 
 let addDeno = deno1 * deno2; // 합한 분모
 let addNum = num1 * deno2 + num2 * deno1; // 합한 분자
-let gcd = gcdFunc(addDeno, addNum); // 최대공약수
+let gcd = gcdFunc(addNum, addDeno); // 최대공약수
 
 console.log(gcd ? `${addNum / gcd} ${addDeno / gcd}` : `${addNum} ${addDeno}`);
